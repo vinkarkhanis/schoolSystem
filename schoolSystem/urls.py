3c209e5e89c3551657061a24e156718d67dc3861
CNCSAndroid <nlcompsolutions@gmail.com>
2019-09-16 23:14:52 +0530

    Base code for Login functionality

--- schoolSystem/urls.py
+++ schoolSystem/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('',include('studentinfo.urls')),
     path('admin/', admin.site.urls),
+    path('useraccounts/',include('user_accounts.urls'))
 ]
---
+++ user_accounts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserAccountsConfig(AppConfig):
+    name = 'user_accounts'
+
