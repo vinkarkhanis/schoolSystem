fa0d96c0a2989a72ab568f7964dbf145607c4c38
CNCSAndroid <nlcompsolutions@gmail.com>
2019-09-29 16:20:56 +0530

    functionality improvement

--- studentinfo/models.py
+++ studentinfo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+from . import ConfigReader
 
 
 class Fees(models.Model):
@@ -14,18 +14,23 @@ class Fees(models.Model):
 
 class Student(models.Model):
     rollNum = models.IntegerField()
-    name = models.TextField()
-    gender = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100,blank=True)
+    surname=models.CharField(max_length=100)
+    gender = models.CharField(choices=ConfigReader.gender_reader(),default=ConfigReader.gender_reader()[0],max_length=10)
     address = models.TextField()
-    std = models.IntegerField()
-    div = models.TextField()
-    religion = models.TextField()
-    cast = models.TextField()
-    category = models.TextField()
+    std = models.IntegerField(choices=ConfigReader.standard_reader(), default=ConfigReader.standard_reader()[0])
+    div = models.TextField(choices=ConfigReader.division_reader(), default=ConfigReader.division_reader()[0])
+    religion = models.CharField(max_length=50)
+    cast = models.CharField(max_length=50)
+    category = models.TextField(choices=ConfigReader.category_reader(), default=ConfigReader.category_reader()[0])
     fees=models.FloatField(blank=True, null=True)
 
+    class Meta:
+        unique_together = ('rollNum', 'div','std')
+
     def __str__(self):
-        return  "Name - "+self.name+" --> "+"Class - "+str(self.std)+":"+self.div
+        return  "Name - "+self.first_name+" --> "+"Class - "+str(self.std)+":"+self.div
 
 
 
