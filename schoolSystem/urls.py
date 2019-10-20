12685b974d3c1a4954b4f27369b3b0c3126adc25
sanketbhumkar <sanket.bhumkar4@gmail.com>
2019-10-20 10:46:27 +0530

    Added code for PDF generation

--- schoolSystem/urls.py
+++ schoolSystem/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
-    #path('',include('studentinfo.urls')),
-    path('',admin.site.urls),
+    path('',include('studentinfo.urls')),
+    # path('',admin.site.urls),
     #path('admin/', admin.site.urls),
     path('useraccounts/',include('user_accounts.urls')),
     path('payment/', include('payment.urls')),
--- studentinfo/views.py
+++ studentinfo/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.datetime_safe import datetime
+from django.views import View
+from datetime import datetime
+
+from studentinfo.utils import render_to_pdf
 from .models import Student
 from tablib import Dataset
 from .resources import StudentResource
@@ -9,3 +15,23 @@ def index(request):
 
     return render(request, 'index.html', {'dests': dests})
 
+def GeneratePdf(request):
+    # Retrieve data or whatever you need
+    userName = "Sanket Bhumkar001"
+    now = datetime.now()
+    receiptNo = now.strftime("%d%m%y%H%M")
+    date = now.strftime("%d/%m/%Y")
+    day = now.strftime("%A")
+    studentName = "Sanket"
+    amount = "2000"
+    return render_to_pdf(
+        'pdfTemplate.html',
+        {
+            'pagesize': 'A4',
+            'studentName' : studentName,
+            'amount' : amount,
+            'date': date,
+            'receiptNo': receiptNo,
+            'day' : day
+        }
+    )
