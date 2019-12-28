01ee93465fe95c02064fa85585cef484802fcba3
CNCSAndroid <nlcompsolutions@gmail.com>
2019-12-28 22:02:40 +0530

    functionality improvement

--- payment/signals.py
+++ payment/signals.py
@@ -1,16 +1,23 @@
 from django.db.models.signals import post_save, post_delete, pre_save
-from django.dispatch import receiver
+from django.dispatch import receiver, Signal
 from .models import PaymentInfo
 from studentinfo.models import Student
+from django.dispatch import receiver
+from import_export.signals import post_import, post_export
+
 
 @receiver(post_save, sender=PaymentInfo)
 def create_badge(sender, instance=None, created=False, **kwargs):
+    print("in signal")
     if created:
+        print("in if")
         student, is_created = Student.objects.get_or_create(rollNum=instance.rollNum.rollNum,std=instance.rollNum.std,
                                                             div=instance.rollNum.div)
         student.fees = student.fees-instance.payment
+        print("student is",instance.rollNum.first_name)
         if(student.fees == 0):
-            student.total_fee_received = True;
+            student.total_fee_received = True
+        print("saving student")
         student.save()
 
 @receiver(post_delete, sender=PaymentInfo)
@@ -39,4 +46,8 @@ def update_badge(sender, instance=None, **kwargs):
                 student.save()
         except:
             pass
+
+@receiver(post_import, dispatch_uid='import_payment',sender=PaymentInfo)
+def _post_import(PaymentInfo, **kwargs):
+    create_badge(PaymentInfo)
 
--- studentinfo/signals.py
+++ studentinfo/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Student, Fees
 
@@ -8,23 +8,25 @@ def create_badge(sender, instance=None, created=False, **kwargs):
         fees, is_created = Fees.objects.get_or_create(std=instance.std )
         if(instance.exempt_fee == "Yes"):
             instance.fees = 0
+            instance.total_fee_received = True
         elif(instance.exempt_fee == "No"):
             instance.fees = fees.fees
+            instance.total_fee_received = False
         instance.save()
 
-@receiver(post_save, sender=Student)
-def update_badge(sender, instance=None, **kwargs):
-    student, is_created = Student.objects.get_or_create(rollNum=instance.rollNum, std=instance.std,
-                                                        div=instance.div)
-    fees, is_created = Fees.objects.get_or_create(std=instance.std)
-    try:
-        if(student.exempt_fee == "Yes"):
-            student.total_fee_received = True;
-            student.fees = 0;
-        elif(student.exempt_fee == "No"):
-            student.total_fee_received = False;
-            student.fees = fees.fees
-
-        student.save()
-    except:
-        pass
+# @receiver(post_save, sender=Student)
+# def update_badge(sender, instance=None, **kwargs):
+#     student, is_created = Student.objects.get_or_create(rollNum=instance.rollNum, std=instance.std,
+#                                                         div=instance.div)
+#     fees, is_created = Fees.objects.get_or_create(std=instance.std)
+#     try:
+#         if(student.exempt_fee == "Yes"):
+#             student.total_fee_received = True
+#             student.fees = 0;
+#         elif(student.exempt_fee == "No"):
+#             student.total_fee_received = False
+#             student.fees = fees.fees
+#
+#         student.save()
+#     except:
+#         pass
